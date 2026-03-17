b3d3abbd4ca7671dc310ccde695de250adeb71a2
ryanmachado11 <ryanmizael14@gmail.com>
2026-03-17 19:27:32 -0300

    envio exercicio 4

--- Aula4/ex3/Ex3/Program.cs
+++ Aula4/ex3/Ex3/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bem vindo ao selecionador de classes!\n1 - Mago\n2 - Guerreiro\n3 - Arqueiro");
+            int escolha = Convert.ToInt32(Console.ReadLine());
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("Você escolheu a classe Guerreira! Suas halidades são: Ataque Pesado, Defesa Total");
+                    break;
+                case 2:
+                    Console.WriteLine("Você escolheu a classe Mago! Suas habilidades são: Bola de Fogo, Escudo de Gelo");
+                    break;
+                case 3:
+                    Console.WriteLine("Você escolheu a classe Arqueiro! Suas habilidades são: Flecha Precisa, Disparo Triplo");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }            
         }
     }
 }
