1e2514c060ef13bf88422c5c92a0d2e7fc255ac8
ryanmachado11 <ryanmizael14@gmail.com>
2026-06-23 14:09:33 -0300

    feat: aula9\ex1

--- Aula4/ex1/Ex1/obj/Debug/net9.0/Ex1.AssemblyInfo.cs
+++ Aula4/ex1/Ex1/obj/Debug/net9.0/Ex1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c264795cf5d7561cb27e628a5053412b303e87f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15aacbdd0eb70789fd106e286a85c367bc5b7847")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aula9/Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+ static void Main()
+ {
+    List<int> notas = new();
+    int nota;
+    int media;
+    int contadormedia = 0;
+    int maiorNota = 0;
+    int menorNota = 0;
+    
+    for(int i=0; i<5; i++)
+    {
+        Console.WriteLine("Insira a primeira nota da lista");
+        nota = int.Parse(Console.WriteLine());
+        notas.Add(nota);
+
+        if(nota>maiorNota){ maiorNota = nota; }
+        if(nota<menorNota){ menorNota = nota; }
+    }
+ }
+}
